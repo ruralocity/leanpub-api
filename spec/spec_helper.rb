45ff7a51fb61682c9e810c801d73ba4732ec2689
Aaron Sumner <asumner@mac.com>
2014-02-12 23:43:52 -0600

    Scrub sensitive data from summary cassette

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -10,6 +10,14 @@ Dotenv.load
 VCR.configure do |config|
   config.cassette_library_dir = 'spec/fixtures/leanpub_cassettes'
   config.hook_into :webmock
+
+  config.filter_sensitive_data("<LEANPUB_BOOK_SLUG>") do
+    book_slug
+  end
+
+  config.filter_sensitive_data("<LEANPUB_API_KEY>") do
+    api_key
+  end
 end
 
 def book_slug
